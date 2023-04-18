80c013356942fd1dec0bf687cef9765bee2b6186
RimidalU <stankevichuuladimiru@gmail.com>
2023-04-19 02:19:20 +0300

    feat: implement mongooDB in project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DeedModule } from './deed/deed.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [DeedModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://rimidalu:${process.env.POSTGRES_PASSWORD}@cluster0.tdpnjmp.mongodb.net/?retryWrites=true&w=majority`,
+    ),
+    DeedModule,
+  ],
 })
 export class AppModule {}

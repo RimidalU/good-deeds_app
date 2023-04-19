994170a7c1293bf0ee2f339f94b300d22e6cc3ef
RimidalU <stankevichuuladimiru@gmail.com>
2023-04-19 15:37:20 +0300

    refactor: change type id array

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose, { HydratedDocument } from 'mongoose';
-import { Deed } from 'src/deed/schemas/deed.schema';
+import mongoose, { HydratedDocument, Types } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
@@ -19,10 +18,10 @@ export class User {
   password: string;
 
   @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Deed' }] })
-  deeps: Deed[];
+  deeds: Types.ObjectId[];
 
   @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] })
-  frends: User[];
+  frends: Types.ObjectId[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);

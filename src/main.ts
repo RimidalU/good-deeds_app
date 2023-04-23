55e623ac6e11bfdd4b9246f3756d27a47735647f
RimidalU <stankevichuuladimiru@gmail.com>
2023-04-23 16:13:56 +0300

    feat: add validation to project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 const start = async () => {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
-
     app.enableCors();
+    app.useGlobalPipes(new ValidationPipe());
 
     await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {

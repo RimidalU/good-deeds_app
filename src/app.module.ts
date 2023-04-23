8a08e05257316ed3093437a18efbfc92edcadc31
RimidalU <stankevichuuladimiru@gmail.com>
2023-04-23 15:45:56 +0300

    feat: add auth entity files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DeedModule } from './deed/deed.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UserModule } from './user/user.module';
     ),
     DeedModule,
     UserModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
